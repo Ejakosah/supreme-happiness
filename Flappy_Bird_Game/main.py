c931d649e9e81411b3048e6e832b5f0def89d28e
Ejakosah <tuo89020@temple.edu>
2024-04-29 22:00:53 -0400

    Reset button

--- Flappy_Bird_Game/main.py
+++ Flappy_Bird_Game/main.py
@@ -35,6 +35,8 @@ pass_pipe = False
 #load images
 bg = pygame.image.load('supreme-happiness/img/bg.png')
 ground_img = pygame.image.load('supreme-happiness/img/ground.png')
+button_img = pygame.image.load('supreme-happiness/img/restart.png')
+
 
 def draw_text(text, font, text_col, x, y):
 	img = font.render(text, True, text_col)
@@ -108,12 +110,38 @@ class Pipe(pygame.sprite.Sprite):
 			self.kill()
 
 
+class Button():
+	def __init__(self, x, y, image):
+		self.image = image
+		self.rect = self.image.get_rect()
+		self.rect.topleft = (x, y)
+
+	def draw(self):
+
+		action = False
+
+		#get mouse position
+		pos = pygame.mouse.get_pos()
+
+		#check if mouse is over the button
+		if self.rect.collidepoint(pos):
+			if pygame.mouse.get_pressed()[0] == 1:
+				action = True
+
+		#draw button
+		screen.blit(self.image, (self.rect.x, self.rect.y))
+
+		return action
+
 bird_group = pygame.sprite.Group()
 pipe_group = pygame.sprite.Group()
 
 flappy = Bird(100, int(screen_height / 2))
 
 bird_group.add(flappy)
+
+#create restart button instance
+button = Button(screen_width // 2 - 50, screen_height // 2 - 100, button_img)
 
 
 
@@ -174,6 +202,11 @@ while run:
 			ground_scroll = 0
 			
 		pipe_group.update()
+
+	#check for game over and reset
+	if game_over == True:
+		button.draw()
+		
 		#event handler 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
